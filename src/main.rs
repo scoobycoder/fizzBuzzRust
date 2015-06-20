89fbc86c21a7e59695da3f0435bb188e2336d735
scoobycoder <scoobycoder@gmail.com>
2015-06-20 17:52:36 -0400

    basic passing need to handle memory better

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,31 @@ fn it_should_fizz_for_3() {
 	assert_eq!("fizz", fizz_buzzer(3))
 }
 
+#[test]
+fn it_should_buzz_for_5() {
+	assert_eq!("buzz", fizz_buzzer(5))
+}
+
+#[test]
+fn it_should_fizzbuzz_for_15() {
+	assert_eq!("fizzbuzz", fizz_buzzer(15))
+}
+
+#[test]
+fn it_should_return_number_for_all_other_numbers() {
+	assert_eq!("1", fizz_buzzer(1))
+}
+
 fn fizz_buzzer(x: i32) -> String {
-	"fizz".to_string()
+	if x % 15 == 0 {
+		"fizzbuzz".to_string()		
+	} else if x % 3 == 0 {
+		"fizz".to_string()	
+	} else if x  % 5 == 0 {
+		"buzz".to_string()
+	} else {
+		x.to_string()
+	}
+
 }
 
